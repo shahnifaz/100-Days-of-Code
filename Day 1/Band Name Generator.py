e439e078aefa656dc542b3e81d0925eff9c3cef0
shahnifaz <88288801+shahnifaz@users.noreply.github.com>
2025-01-18 15:44:02 +0600

    Cleared

--- Day 1/Band Name Generator.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the band name generator")
-
-city = input("What city did you grow up in? : ")
-
-pet = input("Name a pet : ")
-
-print("Your band name could be : " + city + " " + pet)
