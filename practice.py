4edad781f7d3cd3611c46a406cbe419b02102391
shahnifaz <88288801+shahnifaz@users.noreply.github.com>
2025-01-27 20:12:10 +0600

    day 9 done

---
+++ practice.py
@@ -0,0 +1,14 @@
+travel_log = [
+    {
+        "country": "France",
+        "visits": 12,
+        "cities":["Paris", "Lille", "Dijon"]
+    },
+    {
+        "country": "Germany",
+        "visits": 5,
+        "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    },
+]
+
+print(travel_log)
